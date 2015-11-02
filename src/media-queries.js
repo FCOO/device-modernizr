477b4ea6598e6eb068f68fc7b9a4dae75a7d3661
NielsHolt <niels@steenbuchholt.dk>
2015-11-02 11:46:16 +0100

    * No message *

--- src/media-queries.js
+++ src/media-queries.js
@@ -109,3 +109,17 @@
 
 
 }(jQuery, this, document));
+
+(function (window, Modernizr) {
+    'use strict';
+    var md = new MobileDetect(navigator.userAgent),
+        grade = md.mobileGrade();
+    Modernizr.addTest({
+        mobile: !!md.mobile(),
+        phone: !!md.phone(),
+        tablet: !!md.tablet(),
+        mobilegradea: grade === 'A'
+    });
+    window.mobileDetect = md;
+})(window, Modernizr);
+
